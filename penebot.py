3f8b92f44c8aa6ae8e5abf80f095a1bdcf851087
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2014-04-11 07:54:12 +0200

    Refactoring

--- penebot.py
+++ penebot.py
@@ -11,9 +11,9 @@ parser.add_option('-n', '--nick', dest='nick', default='pybot',
     help='set bot nickname')
 parser.add_option('-s', '--ssl', action='store_true', dest='ssl',
     default=False, help='use ssl')
-parser.add_option('-p', '--password', dest='password', default='',
+parser.add_option('-P', '--password', dest='password', default='',
     help='set IRC password to connect')
-parser.add_option('-f', '--plugins-folder', dest='plugins_folder', default='',
+parser.add_option('-p', '--plugins-folder', dest='plugins_folder', default='',
     help='set plugins folder')
 (options, args) = parser.parse_args()
 
@@ -22,7 +22,14 @@ if len(args) == 3:
     bot.set_nick(options.nick)
     bot.set_ssl(options.ssl)
     bot.set_password(options.password)
-    bot.set_plugins_folder(options.plugins_folder)
+
+    if options.plugins_folder:
+        try:
+            bot.load_plugins(options.plugins_folder)
+        except Exception as e:
+            print 'Error:', e
+            sys.exit(1)
+
     try:
         bot.connect(args[0], int(args[1]), args[2])
     except Exception as e:
--- pyrcbot/ircbot.py
+++ pyrcbot/ircbot.py
@@ -16,30 +16,26 @@ class IRCBot:
         self.ssl = True
         self.nick = 'pybot'
         self.password = ''
-        self.plugins_folder = ''
         self.plugins = []
+        self.load_plugins(os.path.dirname(os.path.realpath(__file__))+'/plugins')
         self.socket = None
 
     def parse_data(self, data):
         print 'Received data = {\n%s\n}' % data
-        try:
+        for p in self.plugins:
+            for m in re.finditer(p.get_regexp(), data):
+                ret = p.cmd(m)
+                if ret: self.socket.send('PRIVMSG %s :[+] %s\r\n' % (self.channel, ret))
+        if re.search('^!help', data, re.MULTILINE):
             for p in self.plugins:
-                for m in re.finditer(p.get_regexp(), data):
-                    ret = p.cmd(m)
-                    self.socket.send('PRIVMSG %s :[+] %s\r\n' % (self.channel, ret))
-            if re.search('^!help', data, re.MULTILINE):
-                for p in self.plugins:
-                    self.socket.send('PRIVMSG %s :[+] %s\r\n' % (self.channel, p.get_help()))
-            for m in re.finditer('(PING[^\r\n]*)[\r\n]+', data):
-                self.socket.send('PONG %s\r\n' % m.group(1).split(' ')[1])
-        except Exception:
-            self.socket.send('PRIVMSG %s :[+] Wrong syntax\r\n' % self.channel)
+                self.socket.send('PRIVMSG %s :[+] %s\r\n' % (self.channel, p.get_help()))
+        for m in re.finditer('(PING[^\r\n]*)[\r\n]+', data):
+            self.socket.send('PONG %s\r\n' % m.group(1).split(' ')[1])
 
     def connect(self, server, port, channel):
         self.server = server
         self.port = port
         self.channel = channel
-        self.load_plugins()
 
         if self.ssl:
             self.socket = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
@@ -60,24 +56,24 @@ class IRCBot:
         # Wait for commands
         while True:
             data = recv_timeout(self.socket)
-            self.parse_data(data)
+            try:
+                self.parse_data(data)
+            except Exception:
+                self.socket.send('PRIVMSG %s :[+] Wrong syntax\r\n' % self.channel)
 
     def close(self):
         if self.socket:
             self.socket.close()
             self.socket = None
 
-    def load_plugins(self):
-        if self.plugins_folder:
-            path = self.plugins_folder
-        else:
-            path = os.path.dirname(os.path.realpath(__file__))+'/plugins'
+    def load_plugins(self, path):
         if not path in sys.path:
             sys.path.append(path)
         files = filter(lambda f: f.endswith('.py'), os.listdir(path))
         modules = map(__import__, map(lambda f: os.path.splitext(f)[0], files))
-        self.plugins = map(lambda p: getattr(p, 'IRCPlugin')(), modules)
-        for p in self.plugins:
+        plugins = map(lambda p: getattr(p, 'IRCPlugin')(), modules)
+        self.plugins.extend(plugins)
+        for p in plugins:
             print "Loaded plugin:", p.__class__
 
     def set_nick(self, nick):
@@ -88,6 +84,3 @@ class IRCBot:
 
     def set_password(self, password):
         self.password = password
-
-    def set_plugins_folder(self, folder):
-        self.plugins_folder = folder
