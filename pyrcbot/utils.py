e9572ddb1ac97d970b9ba6345bc46d415930426e
Roi Martin (@nibble_ds) <jroi.martin@gmail.com>
2014-04-12 01:33:45 +0200

    Add LICENSE and README

--- pyrcbot/utils.py
+++ pyrcbot/utils.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+# Copyright 2014 The pyrcbot Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 import time
 
 def recv_timeout(s,timeout=2):
